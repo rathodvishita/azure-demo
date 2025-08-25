c5a7512ef64de4fb3e9972c0fdbff61a5532ef0a
Vishita Rathod <rathodvishita@gmail.com>
2025-08-25 17:01:30 +0530

    change message

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello from Azure Web App! 🚀");
+  res.send("Hello from Azure Web App! After another deployment 🚀");
 });
 
 app.listen(PORT, () => {
